f28f34b227eb4153e8786e9dd643b34ed86a7646
onevcat <onevcat@gmail.com>
2015-06-01 22:38:42 +0900

    Add UniStripe target

---
+++ UniStripe/UniStripe.h
@@ -0,0 +1,13 @@
+//
+//  UniStripe.h
+//  UniStripe
+//
+//  Created by Wei Wang on 15/6/1.
+//  Copyright (c) 2015年 Stripe, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UniStripe : NSObject
+
+@end
---
+++ UniStripe/UniStripe.m
@@ -0,0 +1,13 @@
+//
+//  UniStripe.m
+//  UniStripe
+//
+//  Created by Wei Wang on 15/6/1.
+//  Copyright (c) 2015年 Stripe, Inc. All rights reserved.
+//
+
+#import "UniStripe.h"
+
+@implementation UniStripe
+
+@end
